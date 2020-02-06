5c21a5cd3b768bd400deaeb26222391f1ac1f6d4
vscalcione <vincenzo99.scalcione@gmail.com>
2020-02-06 17:56:09 +0000

    fixing files

--- dade_game.cpp
+++ dade_game.cpp
@@ -3,47 +3,49 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
-
 using namespace std;
-
-int first_launch, second_launch, result;
-char start[2], go[]="go", choice[2], yes[]="yes", no[]="no";
-int first_dade ();
-int second_dade ();
-
-int main(){
-	//system ("color 1F");
-	cout<<"Launch dades - game "<<endl;
-	do{
-		do{
-			cout<<"Type GO for do the first launch"<<endl;
+int n1,n2,a,b,x,y;
+char start[2],go[]="go",choice[2], yes[]="si",no[]="no";
+int primo_dado (int x);
+int secondo_dado (int y);
+main()
+{
+	system ("color 1F");
+	cout<<"Gioco: Lancio dadi "<<endl;
+	do
+	{
+		do
+		{
+			cout<<"Digitare GO per tirare first_index dadi "<<endl;
 			cin>>start;
 		}
 		while (strcmp(go,start)!=0);
-		first_launch = first_dade();
-		second_launch = second_dade();
-		int sum = first_launch + second_launch;
-		cout<<"Result is: "<<sum<<endl;
-		cout<<"Another game-set? ";
-		cin>>choice;
+		a=primo_dado (x);
+		b=secondo_dado (y);
+		cout<<"Il risultato del tuo lancio \212: "<<a+b<<endl;
+		cout<<"Vuoi fare un'altro tiro?: ";
+		cin >> choice;
 		if (strcmp(choice, no) == 0)
+		{
 			break;
+		}
 	}
 	while (strcmp(choice, yes) == 0);
 }
-
-int first_dade (){
-	int result;
-	srand(time(NULL));
-	result = rand() %  6 + 1; 
-	cout<<"First dade: "<<result<<endl;
-	return result;
-}
-
-int second_dade (){
-	int result;
+int primo_dado (int x)
+{
+	cout<<"Primo dado: "<<endl;
 	srand (time(NULL));
-	result = rand() % 6 + 1;
-	cout<<"Second dade: "<<result<<endl;
-	return result;
+	x=1+12+rand()/32767;
+	x=1+12+rand()/32767;
+	cout<<x<<endl;
+	return x;
+}
+int secondo_dado (int y)
+{
+	cout<<"Secondo dado: "<<endl;
+	srand (time(NULL));
+	y=1+12+rand()/32767;
+	y=1+12+rand()/32767;
+	return y;
 }
